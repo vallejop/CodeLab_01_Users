93fa1b6023ffb6f08b6043db3aa9e40114b1bd1b
vallejop <64340141+vallejop@users.noreply.github.com>
2022-03-26 19:25:15 -0500

    Primer commit CodeLab01
    Primer commit CodeLab01 Part 1: Implementing the Users Microservice RESTFUL API

---
+++ src/test/java/com/adaschool/vass/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adaschool.vass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
